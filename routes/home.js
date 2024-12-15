96a11f147af11e22b64108a7970c37472ac37131
sharad patil <sharad>
2024-12-15 19:58:44 +0530

    new

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const Listing = require("../models/listing");  // Make sure the path to the Listing model is correct
 
-// Define the home route
-router.get("/", (req, res) => {
-  res.render("home");  // Render home.ejs from the views folder
+// Home route
+router.get("/", async (req, res) => {
+  try {
+    const allListings = await Listing.find({});  // Fetch all listings from the database
+    res.render("home", { allListings });  // Pass allListings to the home view
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error retrieving listings");
+  }
 });
 
 module.exports = router;
